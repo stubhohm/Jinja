174cad50274608575e4a45be6029a1c75f55c6da
stubhohm <123859155+stubhohm@users.noreply.github.com>
2024-09-21 02:44:30 -0500

    just playing around with primes and getting jinja to say stuff

--- HelloWorld.py
+++ HelloWorld.py
@@ -4,24 +4,51 @@ env = Environment(loader=FileSystemLoader('.'))
 template = env.get_template('HelloWorld.j2')
 
 msg = 'message'
-
 list_items = 'list'
-
-item_1, item_2 , item_3 = 'item 2', 'item 2', 'item 3'
-
+item_1, item_2 , item_3 = 'item 1', 'item 2', 'item 3'
 bread, milk, cheese = 'bread', 'milk', 'cheese'
-
 list_of_stuff = {item_1 : bread,
-        item_2 : milk,
-        item_3 : cheese}
+item_2 : milk,
+item_3 : cheese}
+import time
 
-CTX = {
-    msg: 'Hello World',
-    list_items : list_of_stuff
+start = time.time()
+
+upper = 10000000
+lower = 1
+primes = [1]
+prime_set = set(primes)
+for i in range(lower, upper):
+    is_prime = True
+    for prime_factor in primes:
+        if prime_factor == 1:
+            continue
+        if (prime_factor * prime_factor) > i:
+            break
+        if (i % prime_factor) == 0:
+            #print(f'{i} is not prime because it is divisible by {j}')
+            is_prime = False
+            break
+    if is_prime and i != 1:
+        primes.append(i)
+
+finish = time.time()
+total = finish - start
+
+#print(primes)
+print(f'Total time {total}')
+
+class Context():
+    def __init__(self) -> None:
+        self.upper = upper
+        self.lower = lower
+        
+ctx = Context()
+
+context = {
+    'CTX': ctx
 }
 
-list_of_stuff[item_1]
+rendered_output = template.render(context)
 
-rendered_output = template.render(CTX)
-
-print(rendered_output)
+#print(rendered_output)
