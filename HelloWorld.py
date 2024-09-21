2d602cabc330edba00deae623b1fa9335aad2b27
stubhohm <123859155+stubhohm@users.noreply.github.com>
2024-09-21 03:42:00 -0500

    playing around

--- HelloWorld.py
+++ HelloWorld.py
@@ -10,39 +10,49 @@ bread, milk, cheese = 'bread', 'milk', 'cheese'
 list_of_stuff = {item_1 : bread,
 item_2 : milk,
 item_3 : cheese}
-import time
+upper = 1
+lower = 10000
 
-start = time.time()
+if lower > upper:
+    print(f'{upper} is not lower than {lower}, idoit. Fixed it for you.')
+    a = lower
+    lower = upper
+    upper = a
 
-upper = 10000000
-lower = 1
-primes = [1]
-prime_set = set(primes)
-for i in range(lower, upper):
-    is_prime = True
-    for prime_factor in primes:
-        if prime_factor == 1:
-            continue
-        if (prime_factor * prime_factor) > i:
-            break
-        if (i % prime_factor) == 0:
-            #print(f'{i} is not prime because it is divisible by {j}')
-            is_prime = False
-            break
-    if is_prime and i != 1:
-        primes.append(i)
+def find_primes():
+    import time
+    start = time.time()
+    primes = [1]
+    in_range_primes = []
+    for i in range(1, upper):
+        is_prime = True
+        for prime_factor in primes:
+            if prime_factor == 1:
+                continue
+            if (prime_factor * prime_factor) > i:
+                break
+            if (i % prime_factor) == 0:
+                #print(f'{i} is not prime because it is divisible by {j}')
+                is_prime = False
+                break
+        if is_prime and i != 1:
+            primes.append(i)
+        if is_prime and i >= lower:
+            in_range_primes.append(i)
 
-finish = time.time()
-total = finish - start
+    finish = time.time()
+    total = finish - start
 
-#print(primes)
-print(f'Total time {total}')
+    print(f'{len(in_range_primes)} prime numbers bettween {lower} and {upper}')
+    print(f'Total time {total}')
 
 class Context():
     def __init__(self) -> None:
         self.upper = upper
         self.lower = lower
-        
+
+find_primes()   
+
 ctx = Context()
 
 context = {
